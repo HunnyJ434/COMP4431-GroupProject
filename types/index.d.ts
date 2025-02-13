7a599001cfecf0f94200ee6c494ed6a81472a4e7
pateljanvik <127634173+pateljanvik@users.noreply.github.com>
2025-02-13 17:30:49 -0500

    Add files via upload

--- types/index.d.ts
+++ types/index.d.ts
@@ -8,14 +8,14 @@ declare type SearchParamProps = {
 // ========================================
 
 declare type SignUpParams = {
-  firstName?: string;
-  lastName?: string;
-  address1?: string;
-  city?: string;
-  state?: string;
-  postalCode?: string;
-  dateOfBirth?: string;
-  ssn?: string;
+  firstName: string;
+  lastName: string;
+  address1: string;
+  city: string;
+  state: string;
+  postalCode: string;
+  dateOfBirth: string;
+  ssn: string;
   email: string;
   password: string;
 };
@@ -204,7 +204,7 @@ declare interface BankTabItemProps {
 }
 
 declare interface TotalBalanceBoxProps {
-  accounts: Account[];
+  accounts?: any[];
   totalBanks: number;
   totalCurrentBalance: number;
 }
